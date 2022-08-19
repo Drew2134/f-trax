feb42aefc6b776514c08c53db3991a65b178c201
Drew2134 <a.winchell1896@gmail.com>
2022-08-19 16:56:05 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -31,7 +31,7 @@ require([
             position: {
                 x: -97,
                 y: 38,
-                z: 1000000
+                z: 2500000
             },
             tilt: 30
         }
