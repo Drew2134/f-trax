a48638ae13cf427c77439ce55b7ecc7e1c12b70c
Drew2134 <a.winchell1896@gmail.com>
2022-08-19 19:33:56 -0500

    Update

--- js/main.js
+++ js/main.js
@@ -21,11 +21,27 @@ require([
     esriConfig.apiKey = "AAPKb765a73f61db40b189cd2ec292a872aaUGEazH9qCAdMNXi_0IzSi0RV3jKMpqezs6gUtr8xIRhZTPMnXU8AbU5t3L-WxZFQ";
     
     const aptsGeoJSON = "data/airports.geojson";
-
     const aptsLayer = new GeoJSONLayer({
         url: aptsGeoJSON,
         copyright: "U.S. DOT",
         definitionExpression: "Fac_Type = 'AIRPORT' AND Fac_Use = 'PU'"
+    });
+
+    const allApts = aptsLayer.createQuery();
+    allApts.set({
+        where: "1=1",
+        returnGeometry: false
+    })
+    
+    aptsLayer.queryFeatures(allApts).then((featureSet) => {
+        const features = featureSet.features;
+    })
+
+    const rnwyGeoJSON = "data/runways.geojson";
+    const rnwyLayer = new GeoJSONLayer({
+        url: rnwyGeoJSON,
+        copyright: "U.S. DOT",
+        definitionExpression: "Loc_Id = " + loc_ids
     });
 
     const map = new Map({
