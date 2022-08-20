d979da68bb731ab2b155dc97370a75e53285b581
Drew2134 <a.winchell1896@gmail.com>
2022-08-20 02:01:10 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -183,7 +183,6 @@ require([
 
     function callAPI() {
         $.getJSON("https://opensky-network.org/api/states/all?lamin=45.8389&lomin=5.9962&lamax=47.8229&lomax=10.5226", function(jsonData){
-            console.log(jsonData.states)
             var geoJson = {
                 "type": "FeatureCollection",
                 "name": "Active Flights",
@@ -191,6 +190,7 @@ require([
             };
 
             jsonData.states.forEach((item) => {
+                console.log(item)
                 let geoJSONFeature = {}
                 geoJSONFeature['properties'] = item
                 geoJSONFeature['type'] = "Feature"
