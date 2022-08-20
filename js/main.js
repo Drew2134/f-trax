42bd0649faa8ba93f6ee5ae3e9ba55b79d32a85d
Drew2134 <a.winchell1896@gmail.com>
2022-08-20 02:52:36 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -182,7 +182,7 @@ require([
     CalciteMapArcGISSupport.setSearchExpandEvents(searchWidget);
 
     function callAPI() {
-        $.getJSON("https://opensky-network.org/api/states/all?lamin=45.8389&lomin=5.9962&lamax=47.8229&lomax=10.5226&andrew_winchell:ColtEverett2301!", function(jsonData){
+        $.getJSON("https://opensky-network.org/api/states/all?lamin=45.8389&lomin=5.9962&lamax=47.8229&lomax=10.5226", function(jsonData){
             var geoJson = {
                 "type": "FeatureCollection",
                 "name": "Active Flights",
