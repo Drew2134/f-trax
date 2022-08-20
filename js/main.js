09bda0da1a35ea4e07fc52ff0a9e143b5e0444b4
Drew2134 <a.winchell1896@gmail.com>
2022-08-19 20:51:59 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -33,7 +33,7 @@ require([
         styleName: "EsriIconsStyle",
         name: "Pushpin 1"
     };
-    console.log(aptSymbol, aptSymbol.symbolLayers)
+    console.log(aptSymbol, aptSymbol.color)
     aptSymbol.color = "#1E90FF";
     aptsLayer.renderer = {
         type: "simple",
