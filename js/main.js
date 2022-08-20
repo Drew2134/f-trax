28297a386ab9c6a6dd57220b661e89a2a8d86a96
Drew2134 <a.winchell1896@gmail.com>
2022-08-19 21:12:53 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -36,9 +36,7 @@ require([
     console.log(aptSymbol)
     aptsLayer.renderer = {
         type: "simple",
-        symbol: {
-            "name":"Pushpin 1","type":"PointSymbol3D","symbolLayers":[
-                {"type":"Icon","size":20,"anchor":"bottom","material":{"color":[255,240,0],"transparency":0},"resource":{"href":"https://static.arcgis.com/arcgis/styleItems/Icons/web/resource/Pushpin1.svg"}}]}
+        symbol: aptSymbol
     }
 
     const rnwyGeoJSON = "data/runways.geojson";
