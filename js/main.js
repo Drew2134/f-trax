0776d415655d18a56d05c52ccc862b46fde582b0
Drew2134 <a.winchell1896@gmail.com>
2022-08-21 21:03:04 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -247,7 +247,7 @@ require([
         date = new Date();
         var day = date.getDay(),
             diff = date.getDate() - day + (day == 0 ? -6:1);
-        return Math.floor(new Date(date.setDate(diff)).getTime() / 1000);
+        return Math.floor(new Date(date.setDate(diff).setHours(0, 0, 0, 0)).getTime() / 1000);
     };
 
     function callAPI() {
